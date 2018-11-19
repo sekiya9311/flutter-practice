c7d7f9346c5518254dbaf55ea8e6fb36f1fd5d11
sekiya9311 <sekiya9311@gmail.com>
2018-11-20 00:35:32 +0900

    Startup Name Generator

--- lib/ui/RandomWords.dart
+++ lib/ui/RandomWords.dart
@@ -7,9 +7,40 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator.'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      itemBuilder: (context, idx) {
+        if (idx.isOdd) {
+          return Divider();
+        }
+        final index = idx ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair wPair) {
+    return ListTile(
+      title: Text(
+        wPair.asPascalCase, 
+        style: _biggerFont
+      ),
+    );
   }
 }
